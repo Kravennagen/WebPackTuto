8914f59e6a24a171ae1ad6b7d19769b3b163ff54
flaman_m <flaman_m@etna-alternance.net>
2019-11-07 16:17:34 +0100

    test webpack done

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const DashboardPlugin = require("webpack-dashboard/plugin");
+let config = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./public"),
+        filename: "./bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        },
+        {
+            test: /\.scss$/,
+            use: ExtractTextWebpackPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader', 'postcss-loader'],
+            })
+        }
+        ]
+
+    },
+    plugins: [
+        new ExtractTextWebpackPlugin("styles.css"),
+        new UglifyJSPlugin(),
+        new DashboardPlugin()
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, "./public"),
+        historyApiFallback: true,
+        inline: true,
+        open: true,
+        hot: true
+    },
+    devtool: "eval-source-map"
+
+}
+module.exports = config;
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
